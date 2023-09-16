79c0758fdf53e2a3d140282a006b9614c22ca518
cbabu85 <60637553+cbabu85@users.noreply.github.com>
2023-09-16 18:39:13 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-07d3a50bd29811cd1" 
+  ami           = "ami-05552d2dcf89c9b24" 
   instance_type = "t2.micro" 
-  key_name = "keypairpem"
-  vpc_security_group_ids= ["sg-0ff4024a0cba88baf"]
+  key_name = "BabucKeypair"
+  vpc_security_group_ids= ["sg-00f7cae4b48423f8c"]
   connection {
     type     = "ssh"
     user     = "ec2-user"
-    private_key = file("./keypairpem.pem")
+    private_key = file("./BabucKeypair.pem")
     host     = self.public_ip
   }
   provisioner "remote-exec" {
@@ -19,6 +19,6 @@ resource "aws_instance" "test-server" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Project/my-serverfiles/finance-playbook.yml "
+  command = "ansible-playbook /var/lib/jenkins/workspace/Banking/my-serverfiles/finance-playbook.yml "
   } 
 }
