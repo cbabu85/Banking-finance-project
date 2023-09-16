43dd2ad581be37cfcb5c72ad7f32545ff2cadbdd
cbabu85 <60637553+cbabu85@users.noreply.github.com>
2023-09-16 22:02:39 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "test-server" {
   provisioner "local-exec" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
-/*   provisioner "local-exec" {
+   provisioner "local-exec" {
   command = "ansible-playbook /var/lib/jenkins/workspace/Banking-Project/my-serverfiles/finance-playbook.yml "
-  } */
+  } 
 }
