d4a89165277c79c8ae0bb1334871371fa0977889
cbabu85 <60637553+cbabu85@users.noreply.github.com>
2023-09-17 15:16:21 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "test-server" {
-  ami           = "ami-05552d2dcf89c9b24" 
+  ami           = "ami-0f5ee92e2d63afc18" 
   instance_type = "t2.micro" 
   key_name = "BabucKeypair"
   vpc_security_group_ids= ["sg-00f7cae4b48423f8c"]
   connection {
     type     = "ssh"
-    user     = "ec2-user"
+    user     = "ubuntu"
     private_key = file("./BabucKeypair.pem")
     host     = self.public_ip
   }
